115a5976389b6895b908e6371e9ade88421e2df0
pkro <pkrombach@web.de>
2020-07-11 13:17:17 +0200

    replaced "own" compose with R.compose, not sure why not working

--- functional_utilities.js
+++ functional_utilities.js
@@ -9,27 +9,38 @@ function curry(func) {
   };
 }
 
-function compose() {
-  const funcs = Array.from(arguments).reverse();
-  return (val) => {
-    return funcs.reduce((acc, func) => func(acc), val);
-  };
-}
+// something seems to be wrong with the functions below
+const compose = R.compose;
+// work fine by themselves, but not when used in compose.log (there only R.compose works for some reason)
+// function compose() {
+//   const funcs = Array.from(arguments).reverse();
+//   return (val) => {
+//     return funcs.reduce((acc, func) => func(acc), val);
+//   };
+// }
+
+// function compose(...fns) {
+//   return (x) => {
+//     return R.reduceRight((fn, output) => fn(output), x, fns);
+//   };
+// }
 
 // tap :: Function -> a -> a
+// variant of identity combinator (?)
 const tap = (f) => (x) => {
   f(x);
   return x;
 };
 
-const labelLog = (label) => console.log.bind(console, label);
+const trace = tap(console.log.bind(console));
+// or just const trace = tap(console.log); ?
 
-const trace = compose(tap, labelLog);
-
+// why doesn't this work with "my" compose?
 compose.log = compose(
   R.apply(compose), // compose resulting list
   R.prepend(trace), // prepend resulting list with the trace function so we have the end result
   R.reduce((acc, fn) => [...acc, fn, trace], []), // put trace between each step
+  //R.reduce((acc, fn) => acc.concat(fn, trace), []),
   Array // make an array from the arguments
 );
 
