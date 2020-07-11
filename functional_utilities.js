451a958c854cabcbc23c89b2e89372f04d9598ad
pkro <pkrombach@web.de>
2020-07-11 08:50:30 +0200

    added log method to compose (own compose implementation just for learning reasons)

--- functional_utilities.js
+++ functional_utilities.js
@@ -1,4 +1,6 @@
-export function curry(func) {
+const R = require('ramda');
+
+function curry(func) {
   return (...args) => {
     if (args.length >= func.length) {
       return func.apply(null, args); // apply passes array values as individual parameters (_A_pply passes _A_rray)
@@ -13,3 +15,22 @@ function compose() {
     return funcs.reduce((acc, func) => func(acc), val);
   };
 }
+
+// tap :: Function -> a -> a
+const tap = (f) => (x) => {
+  f(x);
+  return x;
+};
+
+const labelLog = (label) => console.log.bind(console, label);
+
+const trace = compose(tap, labelLog);
+
+compose.log = compose(
+  R.apply(compose), // compose resulting list
+  R.prepend(trace), // prepend resulting list with the trace function so we have the end result
+  R.reduce((acc, fn) => [...acc, fn, trace], []), // put trace between each step
+  Array // make an array from the arguments
+);
+
+module.exports = { curry, compose };
