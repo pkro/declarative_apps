ec1094c7a84dcf23dabe076c8d98ba11e5aa2e40
pkro <pkrombach@web.de>
2020-09-02 07:58:00 +0200

    setup slides action

--- slideshow_project/app/data/reducers.js
+++ slideshow_project/app/data/reducers.js
@@ -1,7 +1,14 @@
 import R from "ramda";
+import { setupSlides } from "../utils/slide-utils";
 // mainRecucer :: (Object, Object) -> Object
 export default function mainReducer(state, action) {
   switch (action.type) {
+    case "SETUP_SLIDES":
+      const presentation = {
+        ...state.presentation,
+        slides: setupSlides(action.value),
+      };
+      return { ...state, presentation };
     case "TEST_ACTION":
       //return R.merge(state, {title: 'Packt Pub Presentation App'})
       return { ...state, title: "PAckt pub presentation app" };
